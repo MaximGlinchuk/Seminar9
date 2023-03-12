d0376223ead396ab39e3fbe4154ebbad0c20f87c
Максим Глинчук <glinchuk.maxim@yandex.ru>
2023-03-12 18:07:51 +0400

    complete task64

---
+++ task64/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string PrintNumber(int start, int end)
+{
+    if (start == end) return start.ToString();
+    return (start + "," + PrintNumber(start - 1, end));
+}
+
+Console.WriteLine(PrintNumber(N,1));
